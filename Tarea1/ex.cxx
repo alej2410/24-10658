5f8ddeb5253dfcac28fc5cc51b74630d9a0daa03
alej2410 <24-10658@usb.ve>
2026-05-15 00:44:43 -0400

    Update ex.cxx

--- Tarea1/ex.cxx
+++ Tarea1/ex.cxx
@@ -1,4 +1,5 @@
 #include <vector>
+#include <cmath>
 
 using namespace std;
 
@@ -7,15 +8,50 @@ int main(){
 }
 
 double mean(vector<double> v){
-  return 0;
+    if (v.empty()) return 0.0;
+    double sum = 0.0;
+    for(double x:v){
+        sum += x;
+    }
+    return sum / v.size();
 }
 
 double variance(vector<double> v){
-  return 0;
+    int n = v.size();
+    if (n < 2) return 0.0;
+    double sum = 0.0;
+    double sum_sq = 0.0;
+
+    for (double x : v){
+        sum    += x;
+        sum_sq += x * x;
+    }
+    return (sum_sq - (sum * sum) / n) / (n - 1);
 }
 
-double pearson_r(vector<double> A, vector<double> B){
-  return 0;
+double pearson_r(vector<double> A, vector<double> B) {
+    if (A.empty() || A.size() != B.size()) return 0.0;
+
+    int n = A.size();
+
+    double sum_A = 0.0, sum_B = 0.0;
+    for (double a : A) sum_A += a;
+    for (double b : B) sum_B += b;
+    double mean_A = sum_A / n;
+    double mean_B = sum_B / n;
+
+    double num = 0.0, den_A = 0.0, den_B = 0.0;
+    for (int i = 0; i < n; i++) {
+        double diff_A = A[i] - mean_A;
+        double diff_B = B[i] - mean_B;
+        num   += diff_A * diff_B;
+        den_A += diff_A * diff_A;
+        den_B += diff_B * diff_B;
+    }
+
+    if (den_A == 0.0 || den_B == 0.0) return 0.0;
+
+    return num / sqrt(den_A * den_B);
 }
 
 vector<char> dec_to_septapus(int n){return {};}
